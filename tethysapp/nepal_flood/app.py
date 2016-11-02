2f9c505dd4999ae42ab5ecbe6c361a77e50b0bfb
Christian Kesler <christiankesler11@gmail.com>
2016-11-02 15:39:37 -0600

    first commit

---
+++ tethysapp/nepal_flood/app.py
@@ -0,0 +1,31 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class NepalFloodMapViewer(TethysAppBase):
+    """
+    Tethys app class for Nepal Flood Map Viewer.
+    """
+
+    name = 'Nepal Flood Map Viewer'
+    index = 'nepal_flood:home'
+    icon = 'nepal_flood/images/icon.gif'
+    package = 'nepal_flood'
+    root_url = 'nepal-flood'
+    color = '#e74c3c'
+    description = 'Place a brief description of your app here.'
+    enable_feedback = False
+    feedback_emails = []
+
+        
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (UrlMap(name='home',
+                           url='nepal-flood',
+                           controller='nepal_flood.controllers.home'),
+        )
+
+        return url_maps
---
+++ tethysapp/nepal_flood/public/js/main.js
@@ -0,0 +1,32 @@
+
+$(function () {
+//Here we are declaring the projection object for Web Mercator
+var projection = ol.proj.get('EPSG:3857');
+
+//Define Basemap
+//Here we are declaring the raster layer as a separate object to put in the map later
+var baseLayer = new ol.layer.Tile({
+    source: new ol.source.BingMaps({
+        key: '5TC0yID7CYaqv3nVQLKe~xWVt4aXWMJq2Ed72cO4xsA~ApdeyQwHyH_btMjQS1NJ7OHKY8BK-W-EMQMrIavoQUMYXeZIQOUURnKGBOC7UCt4',
+        imagerySet: 'AerialWithLabels'  // Options 'Aerial', 'AerialWithLabels', 'Road'
+        })
+    });
+
+layers = [baseLayer];
+
+//Establish the view area. Note the reprojection from lat long (EPSG:4326) to Web Mercator (EPSG:3857)
+var view = new ol.View({
+        center: [9350837, 3298918],
+        projection: projection,
+        zoom: 7,
+    })
+
+//Declare the map object itself.
+var map = new ol.Map({
+    target: document.getElementById("map"),
+    layers: layers,
+    view: view,
+});
+
+map.addControl(new ol.control.ZoomSlider());
+});
