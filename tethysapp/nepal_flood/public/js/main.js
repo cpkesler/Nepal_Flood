ecba3ad7a73551cd41bb5e70c922cf22adee79a4
Christian Kesler <christiankesler11@gmail.com>
2016-11-30 19:51:50 -0700

    hires and lowres

--- tethysapp/nepal_flood/public/js/main.js
+++ tethysapp/nepal_flood/public/js/main.js
@@ -22,22 +22,37 @@ var FloodMap =  new ol.source.TileWMS({
         crossOrigin: 'Anonymous' //This is necessary for CORS security in the browser
         });
 
+var FloodMapHiRes =  new ol.source.TileWMS({
+        url:'',
+
+        params:{
+            LAYERS:"0",
+//            FORMAT:"image/png", //Not a necessary line, but maybe useful if needed later
+        },
+        crossOrigin: 'Anonymous' //This is necessary for CORS security in the browser
+        });
+
 
 var flood = new ol.layer.Tile({
     source:FloodMap
     }); //Thanks to http://jsfiddle.net/GFarkas/tr0s6uno/ for getting the layer working
 
+var floodHiRes = new ol.layer.Tile({
+    source:FloodMapHiRes
+    }); //Thanks to http://jsfiddle.net/GFarkas/tr0s6uno/ for getting the layer working
+
 //Set opacity of layers
 flood.setOpacity(0.8);
+floodHiRes.setOpacity(0.8);
 
-sources = [FloodMap];
-layers = [baseLayer, flood];
+sources = [FloodMap, FloodMapHiRes];
+layers = [baseLayer, flood, floodHiRes];
 
 //Establish the view area. Note the reprojection from lat long (EPSG:4326) to Web Mercator (EPSG:3857)
 var view = new ol.View({
-        center: [9159517, 3238918],
+        center: [9111517, 3258918],
         projection: projection,
-        zoom: 10,
+        zoom: 11,
     })
 
 //Declare the map object itself.
@@ -71,18 +86,22 @@ $(function() {
     $( "#slider" ).slider({
       value:0,
       min: 0,
-      max: 10,
+      max: 8,
       step: 1,
       slide: function( event, ui ) {
         $( "#amount" ).val( ui.value );
         var decimal_value = ui.value.toString().split(".").join("")
         if (ui.value != 0) {
             var url = 'http://geoserver.byu.edu/arcgis/services/Nepal_West/Nepal_' + decimal_value + '/MapServer/WmsServer?';
+            var url1 = 'http://geoserver.byu.edu/arcgis/services/Nepal_West_HiRes/Nepal_' + decimal_value + '/MapServer/WmsServer?';
            }
         else {
             var url = ''
+            var url1 = ''
         }
             FloodMap.setUrl(url);
+            FloodMapHiRes.setUrl(url1);
+
       }
     });
     $( "#amount" ).val( $( "#slider" ).slider( "value" ) );
